fa88f2f7804efc759d97906dbf9cfe95ca7815d7
sudhanshumishra604 <sudhanshumishra604@gmail.com>
2025-07-12 06:58:23 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144523
+let accountemail = "sudhanshu@123"
+var accountpassword = "1245679"
+accountcity = "Jaipur"
+let accountstate;
+
+// accountId = 2     //not allowed
+
+accountpassword = "111"
+accountcity = "Banglore"
+accountemail = "shreya@334"
+
+// prefer not to use var because of issue in block scope and function....
+
+console.table([accountId,accountpassword,accountcity,accountemail.accountstate]);
+
+console.log(accountId);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  // Treats all js code as newer version...
+
+//alert(3+3)   // We are using node js, not the browser..
+
+console.log(3+3);  console.log("Sudhanshu")  //code readibility shold be high
+
+let name = "Sudhanshu"  //String
+let age = 16   // number
+let loggedIN = true   //boolean
+
+//datatypes :-
+
+//number => 2 to power 53
+//String => ""
+// bigint => used to store very large numbers
+//boolean => true/false
+//null =>  its a type  (standalone value)
+// undefined => when datatype is not defined for a variable
+// symbol => uniqueness
+//object =>
+
+console.log(typeof "hitesh");
+console.log(typeof null);  // returns object
+console.log(typeof undefined);  //undefined
